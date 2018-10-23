155977bae8b1bd56b17945958a1ff73cb6953215
Di Jin <332548128@qq.com>
2018-10-22 19:08:23 -0500

    google map util

---
+++ flickerpic.php
@@ -0,0 +1,62 @@
+<form action="flickerpic.php">
+<input type="text" name="search" value="<?php echo $_GET['search'] ?>">
+<input type="submit" value="Search">
+</form>
+<?php
+class Flickr { 
+ 
+ private $apiKey; 
+ 
+ public function __construct($apikey = null) {
+ $this->apiKey = $apikey;
+ } 
+ 
+ public function search($query = null, $user_id = null, $per_page = 50, $format = 'php_serial') { 
+ $args = array(
+ 'method' => 'flickr.photos.search',
+ 'api_key' => $this->apiKey,
+ 'text' => urlencode($query),
+ 'user_id' => $user_id,
+ 'per_page' => $per_page,
+ 'format' => $format,
+ 'sort' =>'date-posted-desc'
+ );
+ $url = 'http://flickr.com/services/rest/?'; 
+ $search = $url.http_build_query($args);
+ $result = $this->file_get_contents_curl($search); 
+ if ($format == 'php_serial') $result = unserialize($result); 
+ return $result; 
+ } 
+ 
+ private function file_get_contents_curl($url) {
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+ curl_setopt($ch, CURLOPT_HEADER, 0);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
+ curl_setopt($ch, CURLOPT_URL, $url);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $data = curl_exec($ch);
+ $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+ curl_close($ch);
+ if ($retcode == 200) {
+ return $data;
+ } else {
+ return null;
+ }
+ } 
+}
+$search = $_GET["search"];
+if($search)
+{
+$Flickr = new Flickr('78dc2b13d9cc1127139a1aa12bf9fcc6'); 
+$data = $Flickr->search($search,null,50,'php_serial'); 
+	foreach($data['photos']['photo'] as $photo) 
+	{ 
+	 /* 
+	 * https://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
+	 */
+	 echo '<a href="https://www.flickr.com/photos/' .$photo["owner"]. '/' .$photo["id"]. '" target="_blank"><img src="' . 'http://farm' . $photo["farm"] . '.static.flickr.com/' . $photo["server"] . '/' . $photo["id"] . '_' . $photo["secret"] . '_t.jpg"></a>'; 
+	} 
+}
+?>
